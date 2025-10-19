ee7cc7a33e215c93d2e49dcfc06691c9fb755d00
Tarik Ouabrk <tarikouabrk0@gmail.com>
2025-10-19 23:36:35 +0100

    done

--- challenge/src/main/java/student/Test.java
+++ challenge/src/main/java/student/Test.java
@@ -7,24 +7,24 @@ public class Test {
         Major ai = new Major("42", "Artificial Intelligence");
 
         // Create students
-        Student s1 = new Student("Amal", "Safi", "0600000001", "amal.safi@gmail.com", "22885676", cs);
-        Student s2 = new Student("Samir", "Alami", "0600000002", "samir.alami@gmail.com", "23585976", cs);
-        Student s3 = new Student("Youssef", "Berrada", "0600000003", "y.berrada@gmail.com", "24585976", ai);
+        Student s1 = new Student("Amal", "Safi", "0600112001", "amal.safi@gmail.com", "29885676", cs);
+        Student s2 = new Student("Samir", "Alami", "0645067002", "samir.alami@gmail.com", "26585976", cs);
+        Student s3 = new Student("Youssef", "Berrada", "0602300103", "youssef.berrada@gmail.com", "24585976", ai);
 
         // Display CS students
         cs.displayStudents();
 
         // Example: find student
-        System.out.println("\nSearching for CNE 23585976: ");
-        Student found = cs.findStudentByCNE("23585976");
+        System.out.println("\nSearching for CNE 26585976: ");
+        Student found = cs.findStudentByCNE("26585976");
         if (found != null)
             System.out.println("Found: " + found.getFullNameFormatted());
         else
             System.out.println("Not found");
 
         // Remove student
-        System.out.println("\nRemoving student 22885676...");
-        cs.removeStudent("22885676");
+        System.out.println("\nRemoving student 29885676...");
+        cs.removeStudent("29885676");
         cs.displayStudents();
 
         // Occupancy rate
